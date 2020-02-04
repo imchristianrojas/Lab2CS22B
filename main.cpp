4b8d3e69d88ab31e612892c608a2bcbf8f3f2908
Christian Rojas <37597196+imchristianrojas@users.noreply.github.com>
2020-02-03 18:59:31 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -67,57 +67,57 @@ int stringNCompare(const char *str1, const char *str2, int num)
 int main(int argc, const char * argv[])
 {
     
-//    //Test Module for the strlen function
-//    char test[] = "Hello World";// Test has 11 chars
-//    char *pTest;
-//    pTest = &test[0];
-//    cout<<"Hello WO"<<endl;
-//    stringLength(pTest);
+   //Test Module for the strlen function
+   char test[] = "Hello World";// Test has 11 chars
+   char *pTest;
+   pTest = &test[0];
+   cout<<"Hello WO"<<endl;
+   stringLength(pTest);
     
     
-//    This needs a little more fine tuning works but needs to check overflow
-//    //Test Module for StringNCopy Function
-//    char test2[] = "Hello World";//Source
-//    char test3[20];// Destination
-//
-//    char *pDestination;
-//    pDestination = &test3[0];
-//    char *pSource;
-//    pSource = &test2[0];
-//
-//    StringNCopy(pDestination, pSource, 7);
-//
-//
+   This needs a little more fine tuning works but needs to check overflow
+   //Test Module for StringNCopy Function
+   char test2[] = "Hello World";//Source
+   char test3[20];// Destination
+
+   char *pDestination;
+   pDestination = &test3[0];
+   char *pSource;
+   pSource = &test2[0];
+
+   StringNCopy(pDestination, pSource, 7);
+
+
     
     /* Need to check bounds somewhow and do somehting about the remainder of left overs spots /0? */
     //Test Module for StringNAdd
-//    char source[50] = "Monkey ";
-//    char destination[50] = "Hello ";
-//
-//    char *pSource = &source[0];
-//    char *pDestination = &destination[0];
-//    char *finalWord;
-//
-//    finalWord = StringNAdd(pDestination, pSource, 3);
-//    for(int i = 0; i<14; i++)
-//        cout<<*(finalWord+i);
+   char source[50] = "Monkey ";
+   char destination[50] = "Hello ";
+
+   char *pSource = &source[0];
+   char *pDestination = &destination[0];
+   char *finalWord;
+
+   finalWord = StringNAdd(pDestination, pSource, 3);
+   for(int i = 0; i<14; i++)
+       cout<<*(finalWord+i);
     
     
     
     /* Test with more data*/
-//    //Test Module for StringNCompare
-//
-//    char compare1[] = "Hello";
-//    char compare2[] = "Hello";
-//
-//    char *pCompare1 = &compare1[0];
-//    char *pCompare2 = &compare2[0];
-//
-//
-//    int check = stringLength(pCompare2);
-//    cout<<check<<endl;
-//
-//
+   //Test Module for StringNCompare
+
+   char compare1[] = "Hello";
+   char compare2[] = "Hello";
+
+   char *pCompare1 = &compare1[0];
+   char *pCompare2 = &compare2[0];
+
+
+   int check = stringLength(pCompare2);
+   cout<<check<<endl;
+
+
     
     
     /*To do list: make menu and test with more data. Amount to test should be around 50 times or more*/
